95739a2e77e31ca87e57be5ee83177f45430e2cd
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2019-10-31 18:49:21 +0100

    only run rh.exe if it exists

--- Dolly/Push.fs
+++ Dolly/Push.fs
@@ -4,7 +4,7 @@ module Dolly.Push
 open System.Net.Http
 open Newtonsoft.Json.Linq
 
-let endpoint = "https://outlook.office.com/webhook/fad37f7c-a3e6-42d9-b77e-8556115b6dab@00f6d248-8400-431b-b006-63cc85dc46c2/IncomingWebhook/3b96b844827b4977b094bec857a93a32/acc01fe7-e32a-49f5-908d-cbb95bc155e6"
+let endpoint = "https://outlook.office.com/webhook/fad37f7c-a3e6-42d9-b77e-8556115b6dab@00f6d248-8400-431b-b006-63cc85dc46c2/IncomingWebhook/c625d98de23940dd8a604cb13152a167/acc01fe7-e32a-49f5-908d-cbb95bc155e6"
 let endpointTest = "https://outlook.office.com/webhook/a3c2b56e-ce0d-48da-9a77-4406dddbad07@00f6d248-8400-431b-b006-63cc85dc46c2/IncomingWebhook/c4f4bfc60b164ca490bc01f520da37ff/acc01fe7-e32a-49f5-908d-cbb95bc155e6"
 let bitbucketEndpoint = "https://bitbucket.brussels.mediagenix.tv/projects/REPDEV/repos/reports/commits/"
 let bla = "bla"
